3c957ddb37b6b84ca46ede9149bb5433fb944455
Gostai <gornostai@list.ru>
2022-10-28 21:07:55 +0300

    migration end

--- src/nft/base/core_impl.rs
+++ src/nft/base/core_impl.rs
@@ -93,9 +93,9 @@ pub struct NonFungibleToken {
 
   // TODO experimental
   pub token_rarity_by_id: Option<LookupMap<TokenId, TokenRarity>>,
-  pub token_collection_by_id: Option<LookupMap<TokenId, TokenCollection>>,
-  pub token_type_by_id: Option<LookupMap<TokenId, TokenType>>,
-  pub token_sub_type_by_id: Option<LookupMap<TokenId, TokenSubType>>,
+  //pub token_collection_by_id: Option<LookupMap<TokenId, TokenCollection>>,
+  //pub token_type_by_id: Option<LookupMap<TokenId, TokenType>>,
+  //pub token_sub_type_by_id: Option<LookupMap<TokenId, TokenSubType>>,
   
   pub token_types_by_id:Option<LookupMap<TokenId, HashMap<String,String>>>,
 
@@ -108,7 +108,9 @@ pub struct NonFungibleToken {
 }
 
 impl NonFungibleToken {
-    pub fn new<Q, R, S, T, R1, B, RM, RT, RTM, E1, E2, E3, E4, E5, U1>(
+    pub fn new<Q, R, S, T, R1, B, RM, RT, RTM, E1, E2, 
+    //E3, E4, E5,
+        U1>(
         owner_by_id_prefix: Q,
         token_metadata_prefix: Option<R>,
         enumeration_prefix: Option<S>,
@@ -123,10 +125,10 @@ impl NonFungibleToken {
         reveal_time_prefix: RTM,
 
         token_rarity_prefix: Option<E1>,
-        token_collection_prefix: Option<E2>,
-        token_type_prefix: Option<E3>,
-        token_sub_type_prefix: Option<E4>,
-        token_types_prefix: Option<E5>,
+        //token_collection_prefix: Option<E2>,
+        //token_type_prefix: Option<E3>,
+        //token_sub_type_prefix: Option<E4>,
+        token_types_prefix: Option<E2>,
         
         upgrade_prefix: Option<U1>,
     )
@@ -143,9 +145,9 @@ impl NonFungibleToken {
             RTM: IntoStorageKey,
             E1: IntoStorageKey,
             E2: IntoStorageKey,
-            E3: IntoStorageKey,
-            E4: IntoStorageKey,
-            E5: IntoStorageKey,
+            //E3: IntoStorageKey,
+            //E4: IntoStorageKey,
+            //E5: IntoStorageKey,
             U1: IntoStorageKey
     {
         let (approvals_by_id, next_approval_id_by_id) = if let Some(prefix) = approval_prefix {
@@ -173,9 +175,9 @@ impl NonFungibleToken {
             token_reveal_time_by_id: LookupMap::new(reveal_time_prefix),
 
             token_rarity_by_id: token_rarity_prefix.map(LookupMap::new),
-            token_collection_by_id: token_collection_prefix.map(LookupMap::new),
-            token_type_by_id: token_type_prefix.map(LookupMap::new),
-            token_sub_type_by_id: token_sub_type_prefix.map(LookupMap::new),
+            //token_collection_by_id: token_collection_prefix.map(LookupMap::new),
+            //token_type_by_id: token_type_prefix.map(LookupMap::new),
+            //token_sub_type_by_id: token_sub_type_prefix.map(LookupMap::new),
             token_types_by_id: token_types_prefix.map(LookupMap::new),
             
             upgrade_prices: upgrade_prefix.map(LookupMap::new),
